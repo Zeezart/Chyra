3d513918454cefc77983315370d757617e213442
Zeezart <azeezhazeezah010@gmail.com>
2024-01-15 12:33:33 -0800

    added footer

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Hero from './hero'
 import Service from './service'
 import Community from './commuinty'
+import Footer from "./footer"
 
 function LandingPage(){
     return(
